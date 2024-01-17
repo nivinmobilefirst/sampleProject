82a6a2d6ff8ea9e949c48ef6baf41e4f0f3d7e99
nivinmobilefirst <nivin.mobilefirst@gmail.com>
2024-01-17 18:01:35 +0530

    added login api

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // Importing required modules
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const jwt = require('jsonwebtoken');
 // Creating an Express application
 const app = express();
+const cors = require('cors');
 
 // Using middleware to parse JSON requests
 app.use(bodyParser.json());
 
+app.use(cors());
 // Sample route to handle GET requests
+
 app.get('/api', (req, res) => {
   res.send('Hello, Express!');
 });
@@ -19,6 +22,34 @@ app.post('/api/data', (req, res) => {
   res.json({ message: 'Data received successfully', data });
 });
 
+const validUser = {
+  username: 'demoUser',
+  password: 'demoPassword',
+};
+const secretKey = 'sdsdsdsdsdww';
+
+app.get('/login', (req, res) => {
+
+  if (validUser.username && validUser.password) {
+    // Generate a token
+    const token = jwt.sign({ username: validUser.username }, secretKey, { expiresIn: '1h' });
+
+    const response = {
+      status: 201,
+      message: 'Login Successfully',
+      token: token,
+    };
+
+    res.status(response.status).json(response);
+  } else {
+    const response = {
+      status: 401,
+      message: 'Authentication Failed',
+    };
+
+    res.status(response.status).json(response);
+  }
+});
 // Setting up the server to listen on port 3000
 const port = 3000;
 app.listen(port, () => {
